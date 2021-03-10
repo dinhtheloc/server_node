90ef43fcd288d34742e0da7079ffb6fb79222319
locdt2 <locdtptit@gmail.com>
2021-03-10 15:10:29 +0700

    init commit

---
+++ app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var app = express();
+
+// view engine setup
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(express.static(path.join(__dirname, 'build', 'build-main')));
+// app.use(express.static(path.join(__dirname, 'build', 'build-other')));
+app.use(express.static(path.join(__dirname, 'build')));
+
+
+
+app.use('/build-other', (req, res, next) => {
+  return res.sendFile(path.resolve( __dirname, 'build', 'build-other', 'index.html'));
+});
+
+app.use('/', (req, res, next) => {
+  return res.sendFile(path.resolve( __dirname, 'public', 'index.html'));
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
